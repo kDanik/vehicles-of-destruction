3e541cc3f6bf716239a33a54a66dd6f4a6e91476
Daniil Kurachkin <86569730+kDanik@users.noreply.github.com>
2022-11-08 22:13:57 +0100

    add utility class for collision impulse calculations

---
+++ Assets/Scripts/Gameplay/Util/CollisionImpulseCalculator.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// This class offers functionality for calculating impulse of 2D colisions.
+/// </summary>
+public static class CollisionImpulseCalculator
+{
+    /// <summary>
+    /// This method should do the same that Collision.impulse does, but for Collision2D.
+    /// Collision2D doesn't have same functionality that Collision (3D) offers.
+    /// </summary>
+    /// <param name="collision">use Collision2D created by OnCollisionEnter2D</param>
+    /// <returns>value of total impulse of given collision</returns>
+    public static float CalculateTotalImpulse(Collision2D collision)
+    {
+        float totalImpulse = 0;
+        ContactPoint2D[] contacts = new ContactPoint2D[collision.contactCount];
+
+        collision.GetContacts(contacts);
+
+        foreach (ContactPoint2D contact in contacts)
+        {
+            totalImpulse += contact.normalImpulse;
+        }
+
+        return totalImpulse;
+    }
+}
