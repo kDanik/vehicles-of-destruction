3153e7fae4e88c88af329a1e7404bc8dda06114b
Daniil Kurachkin <86569730+kDanik@users.noreply.github.com>
2022-11-11 22:57:52 +0100

    small changes to block configurations and added wheel script

---
+++ Assets/Scripts/Blocks/Wheel.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class Wheel : MonoBehaviour
+{
+    [SerializeField]
+    [Tooltip("Wheel object(round thingy) of wheel base")]
+    private GameObject wheel;
+
+    private void OnJointBreak2D(Joint2D joint)
+    {
+        if (joint is WheelJoint2D)
+        {
+            // when wheel joint breaks wheel should be removed from children of this wheel base
+            wheel.transform.parent = transform.parent;
+        }
+    }
+}
