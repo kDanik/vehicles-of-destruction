f7bdda59adc8afd8856b58c54ab02f4796b6adbb
Daniil Kurachkin <daniilkurachkin@gmail.com>
2022-12-17 16:06:11 -0800

    Cleanup (comments/naming) in CollisionImpulseCalculator

--- Assets/Scripts/Gameplay/Util/CollisionImpulseCalculator.cs
+++ Assets/Scripts/Gameplay/Util/CollisionImpulseCalculator.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 
 /// <summary>
-/// This class offers functionality for calculating impulse of 2D colisions.
+/// This class contains methods for calculating impulse of 2D colisions.
 /// </summary>
 public static class CollisionImpulseCalculator
 {
@@ -10,13 +10,13 @@ public static class CollisionImpulseCalculator
     /// Collision2D doesn't have same functionality that Collision (3D) offers.
     /// </summary>
     /// <param name="collision">use Collision2D created by OnCollisionEnter2D</param>
-    /// <returns>value of total impulse of given collision</returns>
-    public static float CalculateTotalImpulse(Collision2D collision)
+    /// <returns>total impulse of the collision</returns>
+    public static float CalculateTotalImpulse(Collision2D collision2D)
     {
         float totalImpulse = 0;
-        ContactPoint2D[] contacts = new ContactPoint2D[collision.contactCount];
-
-        collision.GetContacts(contacts);
+        
+        ContactPoint2D[] contacts = new ContactPoint2D[collision2D.contactCount];
+        collision2D.GetContacts(contacts);
 
         foreach (ContactPoint2D contact in contacts)
         {
