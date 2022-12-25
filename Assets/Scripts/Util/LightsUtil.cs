bdc5e5a2ee93fe2aaf393fea8093931a8a5e640d
Daniil Kurachkin <86569730+kDanik@users.noreply.github.com>
2022-12-25 20:19:42 +0100

    Add utility class with methods for light manipulations

---
+++ Assets/Scripts/Util/LightsUtil.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.Rendering.Universal;
+
+public class LightsUtil
+{
+    public static IEnumerator FadeLightBlinkLoop(Light2D light, float targetIntensityOn, float secondsBetweenStates, float intensityChangePerInterval = 0.05f, float intervalSeconds = 0.05f)
+    {
+        while (true)
+        {
+            yield return FadeLightOff(light, intensityChangePerInterval, intervalSeconds);
+            yield return new WaitForSeconds(secondsBetweenStates);
+            yield return FadeLightOn(light, targetIntensityOn, intensityChangePerInterval, intervalSeconds);
+            yield return new WaitForSeconds(secondsBetweenStates);
+        }
+    }
+
+    public static IEnumerator FadeLightOff(Light2D light, float intensityChangePerInterval = 0.05f, float intervalSeconds = 0.05f)
+    {
+        while (light.intensity > 0)
+        {
+            yield return new WaitForSeconds(intervalSeconds);
+            light.intensity -= intensityChangePerInterval;
+        }
+
+        light.intensity = 0f;
+    }
+
+    public static IEnumerator FadeLightOn(Light2D light, float targetIntensity, float intensityChangePerInterval = 0.05f, float intervalSeconds = 0.05f)
+    {
+        while (light.intensity < targetIntensity)
+        {
+            yield return new WaitForSeconds(intervalSeconds);
+            light.intensity += intensityChangePerInterval;
+        }
+
+        light.intensity = targetIntensity;
+    }
+}
