acdbdf1636b3747ec0fafd0e7c91b8ee454e05ab
Daniil Kurachkin <86569730+kDanik@users.noreply.github.com>
2022-12-25 20:21:02 +0100

    Improve control block visuals (sprite, some effects)

--- Assets/Scripts/Blocks/ControlBlock.cs
+++ Assets/Scripts/Blocks/ControlBlock.cs
@@ -1,18 +1,58 @@
+using System;
 using System.Collections;
-using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.Rendering.Universal;
 
 public class ControlBlock : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    [SerializeField]
+    private GameObject lightsContainer;
+
+    [SerializeField]
+    private GameObject screen;
+
+    [SerializeField]
+    private TMP_Text screenText;
+
+    private Rigidbody2D rigidbodyBuffer;
+
+    private void Awake()
     {
-        
+        rigidbodyBuffer = GetComponent<Rigidbody2D>();
+
+        StartCoroutine(UpdateScreenText());
+        StartCoroutine(UpdateLights());
     }
 
-    // Update is called once per frame
-    void Update()
+    private IEnumerator UpdateScreenText()
     {
-        
+        int velocityUnitsPerMin= (int) Math.Round(rigidbodyBuffer.velocity.magnitude);
+        if (velocityUnitsPerMin > 99) velocityUnitsPerMin = 99;
+
+        screenText.text = velocityUnitsPerMin + "";
+
+        yield return new WaitForSeconds(0.2f);
+        StartCoroutine(UpdateScreenText());
+    }
+
+    private IEnumerator UpdateLights()
+    {
+        int childIndex = UnityEngine.Random.Range(0, lightsContainer.transform.childCount);
+
+        Light2D light = lightsContainer.transform.GetChild(childIndex).GetComponent<Light2D>();
+
+        if (light.intensity == 0f)
+        {
+            StartCoroutine(LightsUtil.FadeLightOn(light, 1f, 0.05f));
+        }
+        else
+        {
+            StartCoroutine(LightsUtil.FadeLightOff(light, 0.2f));
+        }
+
+        yield return new WaitForSeconds(0.5f);
+        StartCoroutine(UpdateLights());
     }
 }
