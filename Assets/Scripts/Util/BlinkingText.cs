d8019b6ed93ea96aabd9cd7ecb75b31ff8074a65
kDanik <86569730+kDanik@users.noreply.github.com>
2023-02-22 12:00:53 +0100

    Add docu and fix naming in BlinkingText.cs

--- Assets/Scripts/Util/BlinkingText.cs
+++ Assets/Scripts/Util/BlinkingText.cs
@@ -2,20 +2,25 @@ using System.Collections;
 using UnityEngine;
 using TMPro;
 
+/// <summary>
+/// When added to gameobject this script will periodicly activate and deactive (blinking effect) specified text component
+/// </summary>
 public class BlinkingText : MonoBehaviour
 {
+    [Tooltip("Delay between activated and deactivated states")]
     [SerializeField]
     private float delayBetweenBlinks = 0.5f ;
 
+    [Tooltip("Text component that should blink")]
     [SerializeField]
     private TMP_Text text;
 
     private void Awake()
     {
-        StartCoroutine(GameOverBlink());
+        StartCoroutine(TextBlink());
     }
 
-    private IEnumerator GameOverBlink()
+    private IEnumerator TextBlink()
     {
         while(true)
         {
